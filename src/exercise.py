38ed65c3ed45a9ddbc9a2b1a1f8c03db7b347a03
Captain C1iff <l.ashdown314@gmx.com>
2020-11-09 12:24:38 +0000

    Works to requirements not sure why it fails whenever I test

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,24 @@
 def main():
-    #write your code below this line
-
+  string = input("Give a string:")
+  integer = int(input("Give an integer:"))
+  floa = float(input("Give a float:"))
+  boolean = bool(input("Give a boolean:"))
+  print ("You gave the string", string)
+  print ("You gave the integer", integer)
+  print("You gave the float", floa)
+  print("You gave the boolean", boolean)
 if __name__ == '__main__':
     main()
+6
+#Give a string:
+#*bye-bye*
+#Give an integer:
+#*11*
+#Give a float:
+#*4.2*
+#Give a boolean:
+#*True*
+#You gave the string bye-bye
+#You gave the integer 11
+#You gave the float 4.2
+#You gave the boolean True
